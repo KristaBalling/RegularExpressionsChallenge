7d41b46ec49b0b5ec9d7cc0b90999bb134aa4061
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-31 14:46:24 -0500

    challenge 4 complete

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,8 @@
 package com.theironyard;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,6 +17,19 @@ public class Main {
         String regExp1 = "I want a (bike|ball).";
         System.out.println(challenge1.matches(regExp1));
         System.out.println(challenge2.matches(regExp1));
+
+        String regExp3 = "I want a \\w+.";
+        Pattern pattern = Pattern.compile(regExp3);
+        Matcher matcher = pattern.matcher(challenge1);
+        System.out.println(matcher.matches());
+
+        matcher = pattern.matcher(challenge2);
+        System.out.println(matcher.matches());
+
+        String challenge4 = "Replace all blanks with underscores";
+        System.out.println(challenge4.replaceAll(" ", "_"));
+        System.out.println(challenge4.replaceAll("\\s", "_"));
+
 
 
 
