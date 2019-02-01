66c7fdc9bf02cd45abf428e6162646431cf862ac
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-01 10:52:00 -0500

    challenge complete

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -80,6 +80,12 @@ public class Main {
         String challenge12 = "11111";
         System.out.println(challenge12.matches("^\\d{5}$"));
 
+        String challenge13 = "11111-1111";
+        System.out.println(challenge13.matches("^\\d{5}-\\d{4}$"));
+
+        System.out.println(challenge12.matches("^\\d{5}(-\\d{4})?$"));
+        System.out.println(challenge13.matches("^\\d{5}(-\\d{4})?$"));
+
 
 
 
